1a7b0f521ade0272c226e2d538683f03a5b44103
Siraphop <6710301036@cdti.ac.th>
2026-07-15 10:25:16 +0700

    edit file week 4 w1 w2 w3 w4 w5

--- Week4/foodcourt_02_gather.py
+++ Week4/foodcourt_02_gather.py
@@ -1,1 +1,25 @@
 # foodcourt_02_gather.py
+import asyncio
+from time import ctime, time
+from food_utils import send_order_to_kitchen
+
+async def main():
+    MY_STUDENT_ID = "6710301036"
+    print(f"{ctime()} | --- [Task 2] Practice using gather to wait for all group orders ---")
+    start_time = time()
+
+    # 1. Spawn 3 concurrent tasks to order food from different shops.
+    t1 = asyncio.create_task(send_order_to_kitchen(MY_STUDENT_ID, "hainanese_chicken", "Chicken Rice Mixed"))
+    t2 = asyncio.create_task(send_order_to_kitchen(MY_STUDENT_ID, "noodle", "Wonton noodle"))
+    t3 = asyncio.create_task(send_order_to_kitchen(MY_STUDENT_ID, "steak", "Sizzling Steak"))
+
+    # 2. Use asyncio.gather to wait for all dishes to be prepared concurrently
+    result = await asyncio.gather(t1, t2, t3)
+
+    for dish in result:
+        print(f"{ctime()} | [Pickup] Shop: {dish['shop']} | Menu: {dish['menu']} is ready")
+
+    print(f"{ctime()} | Total time {time() - start_time:.2f} seconds (Equals to the slowest dish.)")
+
+if __name__ == "__main__":
+    asyncio.run(main())
